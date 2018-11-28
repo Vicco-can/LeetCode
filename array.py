ba56a5b2d56627e1e17348e8269b65e4626ce4a7
Can <zc_gzx@163.com>
2018-11-28 10:10:16 +0800

    提交66、88、118题

--- array.py
+++ array.py
@@ -377,12 +377,151 @@ class Solution6:
         :type digits: List[int]
         :rtype: List[int]
         """
+        length = len(digits)
+        i = 1
+        while i <= length:
+            print(length, i)
+            digits[length-i] += 1
+            if digits[length-i] < 10:
+                return digits
+            else:
+                digits[length - i] = 0
+                if length-i == 0:
+                    digits.insert(0, 1)
+                    return digits
+                else:
+                    i += 1
+
+        return digits
 
 
-'''88'''
+# if __name__ == "__main__":
+#     s = Solution6()
+#     print(s.plusOne([9,9,9]))
 
 
-'''118'''
+'''88、合并两个有序数组
+给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+
+说明:
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+示例:
+
+输入:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+输出: [1,2,2,3,5,6]
+'''
+
+
+class Solution7:
+    def merge(self, nums1, m, nums2, n):
+        """归并排序中的思路
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        i = 0
+        j = 0
+        res = []
+        while i < m and j < n:
+            if nums1[i] < nums2[j]:
+                res.append(nums1[i])
+                i += 1
+            else:
+                res.append(nums2[j])
+                j += 1
+        while i < m:
+            res.append(nums1[i])
+            i += 1
+        while j < n:
+            res.append(nums2[j])
+            j += 1
+        nums1 = res
+        return nums1
+
+
+    def merge1(self, nums1, m, nums2, n):
+        """归并排序中的思路, 合并到一个数组中，
+        因为数组是有序的，假设升序，从后往前遍历比较，将最大的数组放置到nums1的最后
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        index = n + m
+
+        while m > 0 and n > 0:
+            if nums1[m-1] < nums2[n-1]:
+                nums1[index-1] = nums2[n-1]
+                n = n - 1
+            else:
+                nums1[index-1] = nums1[m-1]
+                m = m - 1
+            index -= 1
+        while m > 0:
+            nums1[index-1] = nums1[m-1]
+            m -= 1
+            index -= 1
+        while n > 0:
+            nums1[index-1] = nums2[n-1]
+            n -= 1
+            index -= 1
+        return nums1
+
+#
+# if __name__ == "__main__":
+#     s = Solution7()
+#     print(s.merge1([1,2,3,0,0,0],3,[2,5,6],3))
+
+
+'''118、杨辉三角
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+示例:
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+'''
+
+
+class Solution8:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        res = []
+
+        for i in range(numRows):
+            line = []
+            if i == 0:
+                line.append(1)
+            else:
+                line.append(1)
+
+                for j in range(1, i):
+                    line.append(res[i-1][j-1]+res[i-1][j])
+                line.append(1)
+            res.append(line)
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution8()
+    print(s.generate(5))
 
 '''119'''
 
